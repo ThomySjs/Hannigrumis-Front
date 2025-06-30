3e463942ce2b1e2d773093aeea1e8d7311616a52
ThomySjs <Thomysejas@hotmail.com>
2025-06-30 16:17:37 -0300

    fix: Cambia la ruta de las imagenes.

--- static/js/config.js
+++ static/js/config.js
@@ -1,8 +1,8 @@
 window.env = {
-    API_URL : "http://127.0.0.1:8080/",
+    API_URL : "",
     CATEGORY_ROUTE : "category/",
     PRODUCT_ROUTE : "product/",
-    IMAGE_ROUTE : "images/",
+    IMAGE_ROUTE : "image/",
     LOGIN_ROUTE : "login",
     CHECK_TOKEN_ROUTE : "valid-user",
     RECOVERY_ROUTE : "reset-password",
